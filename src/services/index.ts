3f465f83d750afadee5ec88ef26cad9e11122cf6
Sam (Macbook) <sqrtmango@gmail.com>
2019-09-11 19:50:28 -0400

    Added ansible playbook.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,18 @@
 import searchRoutes from './search/routes';
 import tttRoutes from './ttt/routes';
+import { Request, Response } from "express";
+import path from 'path';
 
-export default [ ...tttRoutes];
+export default [ 
+  {
+    path: "/hw1.yml",
+    method: "get",
+    handler: async (req: Request, res: Response) => {
+
+      res.status(200).sendFile(path.join(__dirname, '../../hw1.yml'));
+      
+    }
+  },
+  ...tttRoutes
+  
+];
