5cfa51c752dc155badfb1c2ada5b352e4f048c07
Sam (Macbook) <sqrtmango@gmail.com>
2019-10-03 02:41:23 -0400

    Removed logs.

--- src/services/rabbit/RabbitController.ts
+++ src/services/rabbit/RabbitController.ts
@@ -8,7 +8,6 @@ export async function connectChannel(callback: Function) {
 
   const connection = await amqp.connect("amqp://localhost");
   const channel = await connection.createChannel();
-  console.log('Created connection to channel.');
   channel.assertExchange('hw4', 'direct', { durable: true });
   const assertQueue = await channel.assertQueue('', { exclusive: true });
   callback(channel, assertQueue);
@@ -18,7 +17,6 @@ export async function connectChannel(callback: Function) {
 export async function listen(keys: string[], callback: Function){
   connectChannel(
     (channel: Channel, queue : Replies.AssertQueue) => {
-      console.log('start...')
       keys.forEach(
 
         (key, i) => {
@@ -28,9 +26,10 @@ export async function listen(keys: string[], callback: Function){
     
       channel.consume(queue.queue, 
         (msg: ConsumeMessage|null) => {
-          console.log(msg);
           if(msg != null){
             callback(msg.content.toString());
+            channel.cancel(queue.queue);
+            
           }
         },
         {
@@ -51,7 +50,6 @@ export async function speak(key: string, msg: string){
     (channel: Channel, queue : Replies.AssertQueue) => {
 
       channel.publish('hw4', key, Buffer.from(msg));
-      console.log("published key " + key);
 
     }
 
